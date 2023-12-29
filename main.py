cbcecea5cb1d0c5bc7e2ca754f7393c6877402b0
muraliganesh34 <124233312+muraliganesh34@users.noreply.github.com>
2023-12-29 14:40:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import streamlit as st
-#display or write something on to the streamlit.write()
-st.write("hello!")
-st.write("<h1>Hello world</h1>",unsafe_allow_html=True)
-#ing colour of test
-st.write("<h1ss style='color:red;'>hello world</h1>",unsafe_allow_html=True)
-st.file_uploader("upload file")
+import numpy as np
+
+st.set_page_config(layout ="wide")
+st.title("Student Management System")
+st.sidebar.title("Fee Management System")
+
+st.sidebar.subheader("Add Student")
+rollnumber=st.sidebar.number_input("Roll Number")
+name=st.sidebar.text_input("Name")
+fees=st.sidebar.number_input("Fees")
+add=st.sidebar.button("Add")
