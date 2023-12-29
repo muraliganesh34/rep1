b9a10019d99da9fbbba9e478b442a1cc0ea50300
muraliganesh34 <124233312+muraliganesh34@users.noreply.github.com>
2023-12-29 10:16:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import streamlit
 #display or write something on to the streamlit.write()
 streamlit.write("hello!")
+st.write("<h1>Hello world</h1>",unsafe_allow_html=True)
+#ing colour of test
+st.write("<h1ss style='color:red;'>hello world</h1>",unsafe_allow_html=True)
+st.file_uploader("upload file")
