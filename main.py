0fff6c02c4d183bd4c945cfe53ba7cc0ceb149cf
muraliganesh34 <124233312+muraliganesh34@users.noreply.github.com>
2023-12-29 10:18:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import streamlit
+import streamlit as st
 #display or write something on to the streamlit.write()
-streamlit.write("hello!")
+st.write("hello!")
 st.write("<h1>Hello world</h1>",unsafe_allow_html=True)
 #ing colour of test
 st.write("<h1ss style='color:red;'>hello world</h1>",unsafe_allow_html=True)
